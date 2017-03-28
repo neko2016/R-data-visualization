b757a131092c403d62c245840c974675c63934b5
neko2016 <honglok16@hotmail.com>
2017-03-28 19:24:43 +0100

    add a file to check color scheme and hexcode in R

---
+++ check_color.r
@@ -0,0 +1,11 @@
+# import library 
+library("RColroBrewer")
+
+# display all color palette by name
+display.brewer.all()
+
+# display the Spectral palette
+display.brewer.pal(n = 4, name = 'Spectral')
+
+# chech the corresponding hexcode in Spectral palette 
+brewer.pal(n = 4, name = "Spectral")
